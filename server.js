9d4cafa8cb721314c0979613d4651f653139ab3c
mp1pro <mp1pro@gmail.com>
2018-12-25 07:57:07 -0500

    install and test mysql

--- server.js
+++ server.js
@@ -32,6 +32,24 @@ import db from './models/db';
 //initiate count
 let count = 0;
 
+
+//****mySQL****//
+import mysql from 'mysql';
+
+// conect to mysql
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "test"
+});
+
+// test connection
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+//****mySQL-end****//
+
 // test route to make sure everything is working (accessed at GET http://localhost:8082)
 router.get('/', function(req, res) {
     //res.json({ message: 'hooray! welcome to our api!' });
